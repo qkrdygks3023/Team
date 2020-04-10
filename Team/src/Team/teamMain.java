cd0836de08adc349b47b8b1056d862dd4f9acd54
park <qkrdygks3023@naver.com>
2020-04-10 15:40:15 +0900

    호경이 때문에 진짜

--- Team/src/Team/teamMain.java
+++ Team/src/Team/teamMain.java
@@ -3,8 +3,8 @@ package Team;
 public class teamMain {
 
 	public static void main(String[] args) {
-		System.out.println("ㅗㅗㅗㅗㅗ");
-		System.out.println("ㅗㅗ");
+		System.out.println("에휴 답답한 호경아 ");
+		System.out.println("그만 놀아라 ");
 
 	}
 
